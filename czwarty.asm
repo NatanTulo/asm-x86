68b7cb89c01fe64308f4e0a399404b688de9ab3f
NatanTulo <s193527@student.pg.edu.pl>
2025-05-19 12:56:25 +0200

    Dodanie nowego programu "czwarty.asm" z funkcjami wyświetlania znaków i przekształcania liter q-z na cyfry.

---
+++ czwarty.asm
@@ -0,0 +1,89 @@
+;   PROGRAM  "czwarty.asm"
+
+dane SEGMENT 	; segment danych
+tablica db 'aqrstuvwxyzbcdefmnopijqrstuvwxyz$'  ; 32-znakowa tablica z małymi literami zakończona '$'
+nowa_linia db 13, 10, '$'                        ; sekwencja nowej linii
+dane ENDS
+
+rozkazy SEGMENT 'CODE' use16 	; segment zawierający rozkazy programu
+        ASSUME cs:rozkazy, ds:dane
+
+; Podprogram wyświetlający jeden znak na ekranie
+; Wejście: DL - znak do wyświetlenia
+wyswietl_znak PROC
+        mov ah, 2    ; funkcja 2h przerwania 21h - wyświetl znak
+        int 21h
+        ret
+wyswietl_znak ENDP
+
+; Podprogram wyświetlający ciąg znaków na ekranie
+; Wejście: DX - offset tablicy znakowej
+wyswietl_ciag PROC
+        mov ah, 9    ; funkcja 9h przerwania 21h - wyświetl ciąg zakończony '$'
+        int 21h
+        ret
+wyswietl_ciag ENDP
+
+; Podprogram kończący program
+zakoncz_program PROC
+        mov al, 0    ; kod powrotu 0 (prawidłowe zakończenie)
+        mov ah, 4Ch  ; funkcja 4Ch przerwania 21h - zakończ program
+        int 21h
+        ret          ; ten ret nigdy nie zostanie wykonany
+zakoncz_program ENDP
+
+; Podprogram wyświetlający tablicę z zamienionymi literami q-z na cyfry 0-9
+wyswietl_przeksztalcona PROC
+        mov dx, OFFSET tablica
+        
+        ; Przeprowadzamy przekształcenie
+        mov si, dx  ; adres tablicy do SI
+        
+petla_przeksztalcenie:
+        mov al, [si]        ; pobierz znak
+        cmp al, '$'         ; sprawdź czy koniec tablicy
+        je koniec_przeksztalcenia
+        
+        ; Sprawdź czy znak jest w zakresie q-z
+        cmp al, 'q'
+        jb bez_zmian
+        cmp al, 'z'
+        ja bez_zmian
+        
+        ; Zamień literę na cyfrę
+        sub al, 'q'         ; q -> 0, r -> 1, ..., z -> 9
+        add al, '0'         ; Konwersja na ASCII cyfry
+        mov [si], al        ; Zapisz przekształcony znak
+        
+bez_zmian:
+        inc si              ; przejdź do następnego znaku
+        jmp petla_przeksztalcenie
+        
+koniec_przeksztalcenia:
+        ; Wyświetl przekształconą tablicę
+        call wyswietl_ciag
+        ret
+wyswietl_przeksztalcona ENDP
+
+wystartuj:
+        mov ax, SEG dane
+        mov ds, ax
+        
+        ; Wyświetl przekształconą tablicę
+        mov dx, OFFSET tablica
+        call wyswietl_przeksztalcona
+        
+        ; Wyświetl nową linię
+        mov dx, OFFSET nowa_linia
+        call wyswietl_ciag
+        
+        ; Zakończ program
+        call zakoncz_program
+rozkazy ENDS
+
+nasz_stos SEGMENT stack 	; segment stosu
+dw 128 dup (?)
+nasz_stos ENDS
+
+END wystartuj 			; wykonanie programu zacznie się od rozkazu
+				; opatrzonego etykietą wystartuj
